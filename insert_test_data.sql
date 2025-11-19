0d27a02dbcace05513588d0fa7fea362dc425996
KoalaGrylls <147829197+KoalaGrylls@users.noreply.github.com>
2025-11-19 22:21:27 +0000

    add insert test user

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,3 +3,8 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+
+
+USE berties_books;
+
+INSERT INTO users (username, firstName, lastName, email, hashedPassword) VALUES ('gold', 'Gold', 'User', 'gold@example.com', '$2b$10$abc123abc123abc123abc123abc123abc123abc123abc123abc123');
